4a37293fec287dcae40d290bfe386eca1b0e7058
qudalsrnt3x <qudalsrnt3x@gmail.com>
2021-07-16 22:51:20 +0900

    1. 인스타그램 클론코딩 - Add SecurityConfig
    스프링 시큐리티를 오버라이드해서 원하는 URL로 활성화 시켜주기

---
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.cos.photogramstart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration // IoC
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/user/**", "/image/**", "/subscribe/*").authenticated()
+                .anyRequest().permitAll()
+
+                .and()
+
+                .formLogin()
+                .loginPage("/auth/signin")
+                .defaultSuccessUrl("/");
+    }
+}
