aac0667a7699f24f94fe976c8e6ab12e975d9eba
qudalsrnt3x <qudalsrnt3x@gmail.com>
2021-07-17 23:10:15 +0900

    4. 인스타그램 클론코딩 - Bcrypt를 사용하여 비밀번호 암호화
    SecurityConfig에 BCryptPasswordEncoder Bean 등록
    등록 후 AuthService에서 암호화 사용

--- src/main/java/com/cos/photogramstart/service/AuthService.java
+++ src/main/java/com/cos/photogramstart/service/AuthService.java
@@ -3,15 +3,26 @@ package com.cos.photogramstart.service;
 import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service //Ioc, 트랜잭션 관리
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Transactional // 해당 함수가 실행되고 종료되기 전까지 트랜잭션 실행
+    // Write 할 때 실행, Insert, Update, Delete
     public User 회원가입(User user) {
+        // 패스워드 암호화
+        String rowPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rowPassword);
+
+        user.setPassword(encPassword);
+        user.setRole("ROLE_USER");
         // 회원가입 진행
         User userEntity = userRepository.save(user); // DB에 들어간 후에 응답받은 데이터
         return userEntity;
--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -6,6 +6,7 @@ import com.cos.photogramstart.web.dto.auth.SignupRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
