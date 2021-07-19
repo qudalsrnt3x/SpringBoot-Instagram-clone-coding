db1967d06ed8381a80f517e307d2e3901948ab17
qudalsrnt3x <qudalsrnt3x@gmail.com>
2021-07-19 23:16:32 +0900

    9. 인스타그램 클론코딩 - 공통응답 Script 만들기
    클라이언트(사용자)가 보기 편하게 스크립트로 처리해준다.

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.cos.photogramstart.handler;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.util.Script;
 import com.cos.photogramstart.web.dto.CMRespDto;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,7 +14,11 @@ import java.util.Map;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class) // RuntimeExcpetion이 발동하는 모든 Exception을 낚아챈다.
-    public CMRespDto<?> validationException(CustomValidationException e) {
-        return new CMRespDto<>(-1, e.getMessage(), e.getErrorMap());
+    public String validationException(CustomValidationException e) {
+        // 1. CMRespDto, Script 비교 - 클라이언트가 응답
+        // 2. Ajax 통신 - CMRespDto - 개발자
+        // 3. Android 통신 - CMRespDto - 개발자가 응답
+
+        return Script.back(e.getErrorMap().toString());
     }
 }
