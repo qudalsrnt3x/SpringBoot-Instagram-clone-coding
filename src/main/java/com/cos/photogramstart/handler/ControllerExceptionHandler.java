17e8ab7a038ae0dbd3e518999c46c298d5104439
qudalsrnt3x <qudalsrnt3x@gmail.com>
2021-07-19 23:09:51 +0900

    8. 인스타그램 클론코딩 - 에러처리 (응답 DTO 만들어주고 제네릭사용)
    기존의 문제점은 getErrorMap만 리턴해줘서 컨트롤러에서 넘겨준 문자열을 보여줄 수 없었다.
    문자열과 Map을 다 보여주기 위해서 CMRespDto를 만들어준다.
    이 때 공통으로 사용하기 위해 T data 로 변수를 설정해준다.

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.cos.photogramstart.handler;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.web.dto.CMRespDto;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ import java.util.Map;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class) // RuntimeExcpetion이 발동하는 모든 Exception을 낚아챈다.
-    public Map<String, String> validationException(CustomValidationException e) {
-        return e.getErrorMap();
+    public CMRespDto<?> validationException(CustomValidationException e) {
+        return new CMRespDto<>(-1, e.getMessage(), e.getErrorMap());
     }
 }
