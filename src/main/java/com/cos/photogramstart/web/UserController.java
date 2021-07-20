aa1fa7d9cb53d02e7aa976fe54670a6f38a3ad13
qudalsrnt3x <qudalsrnt3x@gmail.com>
2021-07-20 22:25:39 +0900

    12. 인스타그램 클론코딩 - 로그인한 유저의 세션 접근
    1. /auth/signin 경로로 post로 접근한 후
    2. PrincipalDetailsService가 username확인 후
    3. username이 있을 경우 UserDetails를 Implements한 PrincipalDetails 타입 반환
    4. 세션에 저장된다.
    
    저장된 세션 찾는 방법
    Session -> SecurityContextHolder 안에 Authentication 객체로 저장되어있다.
    
    1. @AuthenticationPrincipal PrincipalDetails 로 세션에 바로 접근
    2. Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
    
    PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
    
    principalDetails.getUser(); 로 찾는다.
    
    1번을 추천

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -1,5 +1,9 @@
 package com.cos.photogramstart.web;
 
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +18,15 @@ public class UserController {
     }
 
     @GetMapping("/user/{id}/update")
-    public String update(@PathVariable int id) {
+    public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) { // 세션에 접근가능
+        //1. 어노테이션으로 세션에 바로접근 (추천)
+        System.out.println("세션 정보: " + principalDetails.getUser());
+
+        //2. 직접 세션에 접근하는 방법
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PrincipalDetails mPrincipalDetails = (PrincipalDetails) authentication.getPrincipal();
+        System.out.println("직접 찾은 세션 정보 : " + mPrincipalDetails.getUser());
+
         return "user/update";
     }
 }
