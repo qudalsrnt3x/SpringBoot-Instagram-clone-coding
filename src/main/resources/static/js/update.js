261586b1db5ec04370a2844f221164c3e88b3468
qudalsrnt3x <qudalsrnt3x@gmail.com>
2021-07-29 23:56:12 +0900

    15. 인스타그램 클론코딩 - 회원정보 수정 유효성 검사
    프론트, 유효성검사
    
    1. UserUpdateDto 에서 NotBlank인 애들은 update.jsp 에서 required 속성 넣어준다.
    여기서 button 누르면 먹지 않는다. 이뉴는 type이 submit이 아니기 때문
    button 의 속성을 다 지우고 form 태그 옆에 onsubmit() 함수를 설정
    id와 event를 파라미터로 넣어준다.
    
    2. ajax 익셉션을 넘겨주기 위해서 CustomValidationApiException.java 생성
    
    3. ControllerExceptionHandler 에서 메소드 생성
    이때 리턴타입은 ResponseEntity<>를 사용한다. 이유는 상태코드를 넘길 수 있기 때문
    
    ajax는 상태코드가 있어야 하기 때문에 ResponseEntity를 사용해서 상태코드도 같이 넘겨준다.

--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -1,5 +1,6 @@
 // (1) 회원정보 수정
 function update(userId, event) {
+    event.preventDefault(); // 폼태그 액션 막기
 
     let data = $("#profileUpdate").serialize(); // 폼태그 안에 있는 모든 인풋값이 담김
     console.log(data)
@@ -10,10 +11,11 @@ function update(userId, event) {
         data: data,
         contentType:'application/x-www-form-urlencoded; charset=utf-8',
         dataType: 'json'
-    }).done(res=> { // json을 파싱해서 가져온다. res는 자바스크립트 오브젝트가 된다.
-        console.log("update 성공");
+    }).done(res=> { // json을 파싱해서 가져온다. res는 자바스크립트 오브젝트가 된다. // HttpStatus 상태코드 200번대
+        console.log("성공", res);
         location.href=`/user/${userId}`;
-    }).fail(error=>{
-        console.log("update 실패");
+    }).fail(error=>{ // HttpStatus 상태코드 200번대가 아닐 때
+        alert(JSON.stringify(error.responseJSON.data));
+        //console.log("실패", error.responseJSON.data);
     });
 }
